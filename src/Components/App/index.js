3e22b8f3bfab28525ebac303d566774695e214ef
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-05-22 22:05:55 -0300

    feat: POC Cookies Front

---
+++ src/Components/App/index.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function App() {
+
+    const [userLoginInfo, setUserLoginInfo] = useState({ email: "", password: ""});
+
+    async function login(e) {
+        e.preventDefault();
+        try {
+                const data = { 
+                email: userLoginInfo.email, 
+                password: userLoginInfo.password};
+                const promise = await axios.post("http://localhost:5000/login", {});
+                console.log(promise)
+                    localStorage.setItem("token", `${promise.data}`);
+        } catch (e) {
+            alert(e.response.data);
+            setUserLoginInfo({email: "", password: ""});
+        }
+    } 
+
+    return (
+        <form onSubmit={login}>
+        <input type="email" id="email" value={userLoginInfo.email} placeholder="E-mail" required
+            onChange={(e) => setUserLoginInfo({ ...userLoginInfo, email: e.target.value })} />
+        
+        <input type="password" id="password" value={userLoginInfo.password} placeholder="Senha" required
+            onChange={(e) => setUserLoginInfo({ ...userLoginInfo, password: e.target.value })} />
+
+        <div>
+            <button type="submit" >Entrar</button>
+        </div>
+    </form>
+    )
+}
